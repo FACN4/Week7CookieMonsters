54fc6fd7e583e4edd118218b1571fb71a3cdd6ab
DenisKent <kentdenis6@hotmail.co.uk>
2018-08-15 15:38:57 +0300

    Send message done

--- output/mainAssets/dom.js
+++ output/mainAssets/dom.js
@@ -6,16 +6,63 @@ var userMsg = document.getElementById('userMsg');
 var submitBtn = document.getElementById('submitBtn');
 var messageBoard = document.getElementById('message-board');
 
+var globalMessages = '';
+
+var updateScroll = function updateScroll() {
+  console.log('scroll');
+  console.log(messageBoard.scrollTop);
+  console.log(messageBoard.scrollHeight);
+  messageBoard.scrollTop = messageBoard.scrollHeight;
+};
+
+var buildMsg = function buildMsg(imgURL, username, colour, text) {
+  var post = document.createElement('div');
+  post.className = 'post';
+  var image = document.createElement('img');
+  image.className = 'userimg';
+  image.src = imgURL;
+  image.alt = username;
+  var msgArea = document.createElement('div');
+  var user = document.createElement('div');
+  user.className = 'usertag';
+  user.textContent = username;
+  user.color = colour;
+  var msgText = document.createElement('div');
+  msgText.className = 'messageText';
+  msgText.textContent = text;
+  // Append the elements together
+  msgArea.appendChild(user);
+  msgArea.appendChild(msgText);
+  post.appendChild(image);
+  post.appendChild(msgArea);
+  // Add post to message board
+  messageBoard.appendChild(post);
+};
+
+var updateBoard = function updateBoard() {
+  getXhr('/get-msgs', function (allMessages) {
+    if (JSON.stringify(allMessages) !== JSON.stringify(globalMessages)) {
+      globalMessages = allMessages;
+      console.log(globalMessages);
+      console.log(allMessages);
+      allMessages.forEach(function (msg) {
+        buildMsg(msg.photo_url, msg.name, msg.colour, msg.text);
+      });
+      updateScroll();
+    }
+  });
+};
+
+window.setInterval(updateBoard, 1000);
+
+updateBoard();
+
 submitBtn.addEventListener('click', function () {
   var message = userMsg.value;
   postJsonXHR('/send-msg', message, function (err) {
     if (err) {
       console.log(err);
     }
+    updateBoard();
   });
 });
-
-var updateBoard = function updateBoard() {
-  getXhr('/get-msgs', console.log);
-};
-updateBoard();
--- public/mainAssets/dom.js
+++ public/mainAssets/dom.js
@@ -4,6 +4,58 @@ const userMsg = document.getElementById('userMsg');
 const submitBtn = document.getElementById('submitBtn');
 const messageBoard = document.getElementById('message-board');
 
+let globalMessages = '';
+
+
+const updateScroll = () => {
+  console.log('scroll');
+  console.log(messageBoard.scrollTop);
+  console.log(messageBoard.scrollHeight);
+  messageBoard.scrollTop = messageBoard.scrollHeight;
+};
+
+
+const buildMsg = (imgURL, username, colour, text) => {
+  const post = document.createElement('div');
+  post.className = 'post';
+  const image = document.createElement('img');
+  image.className = 'userimg';
+  image.src = imgURL;
+  image.alt = username;
+  const msgArea = document.createElement('div');
+  const user = document.createElement('div');
+  user.className = 'usertag';
+  user.textContent = username;
+  user.color = colour;
+  const msgText = document.createElement('div');
+  msgText.className = 'messageText';
+  msgText.textContent = text;
+  // Append the elements together
+  msgArea.appendChild(user);
+  msgArea.appendChild(msgText);
+  post.appendChild(image);
+  post.appendChild(msgArea);
+  // Add post to message board
+  messageBoard.appendChild(post);
+};
+
+const updateBoard = () => {
+  getXhr('/get-msgs', (allMessages) => {
+    if (JSON.stringify(allMessages) !== JSON.stringify(globalMessages)) {
+      globalMessages = allMessages;
+      console.log(globalMessages);
+      console.log(allMessages);
+      allMessages.forEach((msg) => {
+        buildMsg(msg.photo_url, msg.name, msg.colour, msg.text);
+      });
+      updateScroll();
+    }
+  });
+};
+
+window.setInterval(updateBoard, 1000);
+
+updateBoard();
 
 submitBtn.addEventListener('click', () => {
   const message = userMsg.value;
@@ -11,11 +63,6 @@ submitBtn.addEventListener('click', () => {
     if (err) {
       console.log(err);
     }
+    updateBoard();
   });
 });
-
-
-const updateBoard = () => {
-  getXhr('/get-msgs', console.log);
-};
-updateBoard();
